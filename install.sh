c5bced26bfe6c1cfb0e17bc67b766af2e4584240
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-04-30 13:40:40 +0900

    あ

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 cd ${SCRIPT_DIR}
-sudo cp -r nks-grub/ /boot/grub2/themes/ 
+sudo cp -r ./nks-grub/ /boot/grub/themes/ 
 sudo nano /etc/default/grub
